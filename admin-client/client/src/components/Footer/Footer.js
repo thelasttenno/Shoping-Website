eed82bb42e7986db164459b1046a3743c61c1b59
thelasttenno <66742986+thelasttenno@users.noreply.github.com>
2021-06-11 18:31:32 -0700

    deleted stuff

--- admin-client/client/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import './Footer.scss';
-function Footer(){
-    return (
-        <footer className= 'footer'>
-            <p>&copy; InStock Inc. All Rights Reserved.</p>
-        </footer>
-    )
-}
-export default Footer;
